c8f48ee79707fa7f46b95413a02b0c7b98208bef
Vehiana Tepuhiarii <tepuhiariivehiana1006@gmail.com>
2024-03-14 14:38:11 -1000

    Ceci est la version 5

--- src/Pack1/Main.java
+++ src/Pack1/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("C'est la version 1 :)");
 		System.out.println("C'est la version 2 :)");
+		System.out.println("C'est la version 5 :)");
 
 
 	}
