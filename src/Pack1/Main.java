3f6a535e8dbcb699253467538e18b0e8a1a410eb
Vehiana Tepuhiarii <tepuhiariivehiana1006@gmail.com>
2024-03-14 14:25:34 -1000

    Ceci est la version 2

---
+++ src/Pack1/Main.java
@@ -0,0 +1,12 @@
+package Pack1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("C'est la version 1 :)");
+		System.out.println("C'est la version 2 :)");
+
+
+	}
+
+}
